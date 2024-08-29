477b673208149af8ad31e23b43d262478584da17
roberto david jimenez posada <ia.davidjimenez@ufg.edu.sv>
2024-08-29 07:52:57 -0600

    Finalizacion de código HTML, Finalización de hoja de estilos, Inicio de lógica del negocio

---
+++ js/converter.js
@@ -0,0 +1,7 @@
+const inputField = document.getElementById("input-temp");
+const fromUnitField = document.getElementById("imput-unit");
+const toUnitField = document.getElementById("output-unit");
+const outputField = document.getElementById("imput-temp");
+const form = document.getElementById("converter");
+
+console.log("roberto david jimenez posada");
